0798fe98437a6ca35ee9413b07bc1f2badf77972
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-08-31 21:03:06 -0500

    [+] Notas Clase 3 formas de vincular CSS & JavaScript

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// alert( 'Hola bienvenidos a la pagina gabo' );
+
+function saluda() {
+    console.log( 'Hola bienvenidos a la pagina gabo' );
+    alert( 'Hola bienvenidos a la pagina gabo' );
+}
